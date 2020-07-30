25b6c99d1065b682851ac794257066f9dbc35386
Marek Szuba <mszuba01@gmail.com>
2020-07-30 15:09:17 +0200

    add all functionality

--- app.js
+++ app.js
@@ -7,26 +7,11 @@ class Book {
   }
 }
 
-
-
-
 // UI Class - Handle UI Tasks
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: 'Book One',
-        author: 'John Doe',
-        isbn: '3434434'
-      },
-      {
-        title: 'Book Two',
-        author: 'Jane Doe',
-        isbn: '45545'
-      }
-    ];
 
-    const books = StoredBooks;
+    const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
@@ -75,14 +60,41 @@ class UI {
 
 
 // Store Class - Handles Storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
 
 
 
 
 // Event - Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
-
 
 
 
@@ -102,6 +114,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Instatiate book 
     const book = new Book(title, author, isbn);
 
+    // Add book to store
+    Store.addBook(book);
+
+
     // Add Book to UI
     UI.addBookToList(book);
 
@@ -116,9 +132,17 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 
 
-
-
 // Event - Remove a Book 
 document.querySelector('#book-list').addEventListener('click', (e) => {
+
+  //remove book from UI
   UI.deleteBook(e.target)
+
+  //Remove book from the store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+
+  // Book remove message
+  UI.showAlert('Book removed', 'succes')
+
 })
